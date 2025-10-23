f1aa2d401f73b793b0ce87ba23e70e82a725270a
howspooky <25248588+howspooky@users.noreply.github.com>
2025-10-23 10:49:05 +0100

    Update variables.nix

--- hosts/Default/variables.nix
+++ hosts/Default/variables.nix
@@ -1,10 +1,10 @@
 {
   # User Configuration
-  username = "zer0"; # Your username (auto-set with install.sh, live-install.sh, rebuild)
+  username = "stuart"; # Your username (auto-set with install.sh, live-install.sh, rebuild)
   desktop = "hyprland"; # Options: hyprland, i3-gaps, gnome, plasma6
   terminal = "kitty"; # Options: kitty, alacritty
   editor = "nixvim"; # Options: nixvim, vscode, helix, doom-emacs, nvchad, neovim
-  browser = "zen"; # Options: firefox, floorp, zen
+  browser = "firefox"; # Options: firefox, floorp, zen
   tuiFileManager = "yazi"; # Options: yazi, lf
   sddmTheme = "astronaut"; # Options: astronaut, black_hole, purple_leaves, jake_the_dog, hyprland_kath
   defaultWallpaper = "kurzgesagt.webp"; # to change wallpaper: SUPER + SHIFT + W
@@ -13,8 +13,8 @@
   games = true; # Whether to enable the gaming module
 
   # Hardware Configuration
-  videoDriver = "nvidia"; # CRITICAL: Choose your GPU driver (nvidia, amdgpu, intel)
-  hostname = "NixOS"; # Your system hostname
+  videoDriver = "amdgpu"; # CRITICAL: Choose your GPU driver (nvidia, amdgpu, intel)
+  hostname = "nixos"; # Your system hostname
 
   # Localization
   clock24h = true; # 24H or 12H clock in waybar
